458f1fbd5680de26b51f11e8bc15ebb5bee7c03b
Aishwarya Sivakumar <aishwaryasivakumar@dyn-209-2-210-216.dyn.columbia.edu>
2022-10-29 19:44:02 -0400

    Seed data population

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Dish.destroy_all
+User.destroy_all
+
 dishes = [
     {"dishname": "Fried Rice", "user_email": "aish", "price": 15.8, "quantity": 10, "cuisine": "Chinese", "description": "World's best fried rice!"},
     {"dishname": "Pav Bhaji", "user_email": "aish", "price": 11.5, "quantity": 5, "cuisine": "Indian", "description": "World's best Paaaav!"}
@@ -13,4 +16,15 @@ dishes = [
 dishes.each do |dish|
     curr_dish = Dish.new(dish)
     curr_dish.save!
+end
+
+
+users = [
+    {"email": "sai", "password": "ram", "name": "sairam", "zipcode": "10027"},
+    {"email": "aish", "password": "warya", "name": "aishwarya", "zipcode": "10027"},
+]
+
+users.each do |user|
+    curr_user = User.new(user)
+    curr_user.save!
 end
