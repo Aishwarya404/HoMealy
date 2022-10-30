5a969448e753fd1b538f76b64778d2a482c82f6d
root <root@LAPTOP-NKTQ8NKB.localdomain>
2022-10-30 17:45:44 -0400

    Added cucumber tests

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,24 @@
+
+def create_user(values)
+	default_values = {name: "Roshan", email: "test@mail.com", password: "pass", zipcode: 10028}
+	values = default_values.merge(values)
+	@test_user = User.new values
+end
+
+
+Given /the following users exist/ do |users_table|
+    users_table.hashes.each do |user|
+      # each returned element will be a hash whose key is the table header.
+      # you should arrange to add that user to the database here.
+      User.create(user)
+    end
+  end
+  
+Then /(.*) seed users should exist/ do | n_seeds |
+    expect(User.count).to eq n_seeds.to_i
+end
+
+Given /^I have an account with email: "(.*)" and password: "(.*)"$/ do |email, password|
+	@test_user =  create_user({email: email, password: password})
+	@test_user.save!
+end
