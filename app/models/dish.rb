7082f25475e4c9e7bf475ff448260f3d81c9ae89
Aishwarya Sivakumar <aishwaryasivakumar@dyn-209-2-210-113.dyn.columbia.edu>
2022-11-16 20:28:48 -0500

    UI changes for better boxes, cspacing

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,6 +4,6 @@ class Dish < ApplicationRecord
     validates :dishname, presence: true, length: { maximum: 30 }
     validates :user_email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
     validates :cuisine, presence: true, length: { maximum: 20 }
-    validates :price, presence: true, length: {maximum: 5}, numericality: true
-    validates :quantity, presence: true, length: {maximum: 5}, numericality: { only_integer: true }
+    validates :price, presence: true, length: {maximum: 5}, numericality: {greater_than: 0}
+    validates :quantity, presence: true, length: {maximum: 5}, numericality: { only_integer: true, greater_than: 0 }
 end
